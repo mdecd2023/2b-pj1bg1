0570d515474ed384be51849c94e9f0d0de618142
ziyi8 <41023208@gm.nfu.edu.tw>
2023-03-09 14:32:55 +0800

    新增41023208

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '設計二乙 41023207 楊舒涵', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '設計二乙 41023207 楊舒涵 \n 設計二乙 41023208 廖子儀 \n', 'tags': '', 'url': 'About.html'}]};
